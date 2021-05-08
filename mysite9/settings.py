464ad78b5d656a92655c2cbb65605fef08f2a289
Ali Mehrali <mehrali.git@gmail.com>
2021-05-09 01:52:15 +0430

    course 40

--- mysite9/settings.py
+++ mysite9/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'extensions',
-    'blog.apps.AccountConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
